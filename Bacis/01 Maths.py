0a9c4eefbadaf91240a998131ab6e89b17362b5d
Dhruv <dhruvranap4521@gmail.com>
2026-01-05 10:40:35 +0530

    Add files via upload

---
+++ Bacis/01 Maths.py
@@ -0,0 +1,24 @@
+a  = int(input("Enter 1st no :" ))
+b  = int(input("Enter 2st no :" ))
+
+c = a+b
+d = a-b
+e = a*b
+
+print("The sum of two no : ",c)
+print("The sub of two no : ",d)
+print("The mul of two no : ",e)
+
+print("---------------")
+print("      Divide      ")
+a  = int(input("Enter 1st no :" ))
+b  = int(input("Enter 2st no (b!=0):" ))
+
+if b==0:
+      print("Error")
+else :
+      print(a/b)
+
+
+
+
---
+++ Bacis/02 Converter.py
@@ -0,0 +1,8 @@
+a = int(input("Enter Hours :" ))
+print(f"{a}hours = {a*60}min.")
+
+b = int(input("Enter Min:" ))
+print(f"{b}min. = {b/60}hours")
+
+c = int(input("Enter Dollars (1 $ = 48 Rs.) :" ))
+print(f"{c}$ = {c*48}Rs.")
