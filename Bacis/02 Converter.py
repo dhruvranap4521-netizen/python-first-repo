4d45f02b33ac26ba8e9798d76dc798ec0db77910
Dhruv <dhruvranap4521@gmail.com>
2026-01-05 18:52:38 +0530

    Update 02 Converter.py

--- Bacis/02 Converter.py
+++ Bacis/02 Converter.py
@@ -7,12 +7,12 @@ print(f"{b}min. = {b/60}hours")
 c = int(input("Enter Dollars (1 $ = 48 Rs.) :" ))
 print(f"{c}$ = {c*48}Rs.")
 
-d = int(input("Enter Ruppes (1 $ = 48 Rs.) :" ))
+d = int(input("Enter Ruppes (1$ = 48Rs.) :" ))
 print(f"{d}Rs. = {d/48}$")
 
-d = int(input("Enter dollars :" ))
-f = d/48
-print(f"{f}$ = {f*70}pound")
+d = int(input("Enter dollars (1$ = 48Rs. & 1 pound = 70Rs.) :" ))
+pound = (d * 48) / 70
+print(f"{d}$ = {pound} pound")
 
 a = int(input("Enter grams :" ))
 print(f"{a}gm = {a/1000}kg")
@@ -26,7 +26,8 @@ print(f"{c}bytes = {c/1000000}MB")
 print(f"{c}bytes = {c/1000000000}GB")
 
 d = int(input("Enter celcius :" ))
-print(f"{d}C = {(9/5*c)+32}F")
+print(f"{d}C = {(9/5*d)+32}F")
 
 e = int(input("Enter Fahrenheit :" ))
 print(f"{e}F = {(e-32)*5/9}C")
+
